b71af9e91ee476efa092aa7b1b85fe66ff412777
Sem Mulder <sem@mulderke.net>
2026-01-01 15:24:19 +0100

    Bump syncthing

--- overlay.nix
+++ overlay.nix
@@ -2,10 +2,10 @@ final: prev:
 {
   syncthing-config-tool = final.buildGoModule rec {
     pname = "syncthing-config-tool";
-    version = "0.0.2";
+    version = "0.0.3";
 
     src = ./.;
-    vendorHash = "sha256-jrGJChEexVmQP5jnUofAbtwB67uX2ysBzA3pFBbn/Og=";
+    vendorHash = "sha256-rHbmo2niZHzEUjKogI6bPRVy0IPGKxNSTrOs3oa/Z0Q=";
 
     meta = with final.lib; {
       description = "Utility for validating and converting Syncthing configurations between JSON and XML.";
